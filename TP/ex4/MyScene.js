d11c839fb669ba88596c1113cbeba23ec3795b16
diogoabnunes <diogoabnunes@gmail.com>
2020-03-23 15:50:42 +0000

    error in textures

--- TP/ex4/MyScene.js
+++ TP/ex4/MyScene.js
@@ -25,6 +25,7 @@ class MyScene extends CGFscene {
         this.axis = new CGFaxis(this);
         this.quad = new MyQuad(this);
         this.tangram = new MyTangram(this);
+        this.cube = new MyUnitCubeQuad(this);
 
         //------ Applied Material
         this.quadMaterial = new CGFappearance(this);
@@ -45,8 +46,12 @@ class MyScene extends CGFscene {
         //-------Objects connected to MyInterface
         this.displayAxis = true;
         this.displayQuad = false;
-        this.displayTangram = true;
-        this.scaleFactor = 5;
+        this.displayTangram = false;
+        this.displayCube = true;
+
+        this.linear = true;
+
+        this.scaleFactor = 1.5;
         this.selectedTexture = -1;        
         this.wrapS = 0;
         this.wrapT = 0;
@@ -121,13 +126,24 @@ class MyScene extends CGFscene {
         // Default texture filtering in WebCGF is LINEAR. 
         // Uncomment next line for NEAREST when magnifying, or 
         // add a checkbox in the GUI to alternate in real time
-        
-        // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+
+        if (!this.linear)
+                this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+            else
+                this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+
         if (this.displayQuad)
             this.quad.display();
 
         if (this.displayTangram)
             this.tangram.display();
+
+        if (this.displayCube) {
+
+            this.cube.display();
+        }
+
+        
                     
         // ---- END Primitive drawing section
     }
--- TP/ex4/MyUnitCubeQuad.js
+++ TP/ex4/MyUnitCubeQuad.js
@@ -6,14 +6,43 @@
 class MyUnitCubeQuad extends MyQuad {
     constructor(scene) {
         super(scene);
+        this.initMaterials(this.scene);
 
         this.face = new MyQuad(this.scene);
     }
+    initMaterials(scene) {
+
+        this.topTexture = new CGFappearance(scene);
+        this.topTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.topTexture.setDiffuse(1, 1, 1, 1);
+        this.topTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.topTexture.setShininess(10);
+        this.topTexture.loadTexture('images/mineTop.png');
+        this.topTexture.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.sideTexture = new CGFappearance(scene);
+        this.sideTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.sideTexture.setDiffuse(1, 1, 1, 1);
+        this.sideTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.sideTexture.setShininess(10);
+        this.sideTexture.loadTexture('images/mineSide.png');
+        this.sideTexture.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.bottomTexture = new CGFappearance(scene);
+        this.bottomTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.bottomTexture.setDiffuse(1, 1, 1, 1);
+        this.bottomTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.bottomTexture.setShininess(10);
+        this.bottomTexture.loadTexture('images/mineBottom.png');
+        this.bottomTexture.setTextureWrap('REPEAT', 'REPEAT');
+    }
+
     display() {
 
         // face 1: "front" face
         this.scene.pushMatrix();
         this.scene.translate(0, 0, 0.5);
+        this.sideTexture.apply();
         this.face.display();
         this.scene.popMatrix();
 
@@ -21,6 +50,7 @@ class MyUnitCubeQuad extends MyQuad {
         this.scene.pushMatrix();
         this.scene.translate(0, 0, -0.5);
         this.scene.rotate(Math.PI, 1, 0, 0); // 180º
+        this.sideTexture.apply();
         this.face.display();
         this.scene.popMatrix();
 
@@ -28,6 +58,7 @@ class MyUnitCubeQuad extends MyQuad {
         this.scene.pushMatrix();
         this.scene.translate(0, 0.5, 0);
         this.scene.rotate(-90 * Math.PI/180, 1, 0, 0);
+        this.topTexture.apply();
         this.face.display();
         this.scene.popMatrix();
 
@@ -35,6 +66,7 @@ class MyUnitCubeQuad extends MyQuad {
         this.scene.pushMatrix();
         this.scene.translate(0, -0.5, 0);
         this.scene.rotate(90 * Math.PI/180, 1, 0, 0);
+        this.bottomTexture.apply();
         this.face.display();
         this.scene.popMatrix();
 
@@ -42,6 +74,7 @@ class MyUnitCubeQuad extends MyQuad {
         this.scene.pushMatrix();
         this.scene.translate(-0.5, 0, 0);
         this.scene.rotate(-90 * Math.PI/180, 0, 1, 0);
+        this.sideTexture.apply();
         this.face.display();
         this.scene.popMatrix();
 
@@ -49,6 +82,7 @@ class MyUnitCubeQuad extends MyQuad {
         this.scene.pushMatrix();
         this.scene.translate(0.5, 0, 0);
         this.scene.rotate(90 * Math.PI/180, 0, 1, 0);
+        this.sideTexture.apply();
         this.face.display();
         this.scene.popMatrix();
     }
